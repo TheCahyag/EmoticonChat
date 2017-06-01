650f71eb3526ca8461a9128dbc0f4024d1a94f09
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-06-01 11:46:30 -0400

    Placeholder messages were put in, no implementation

--- src/main/java/com/servegame/bl4de/EmoticonChat/commands/EC/EC.java
+++ src/main/java/com/servegame/bl4de/EmoticonChat/commands/EC/EC.java
@@ -1,18 +1,26 @@
 package com.servegame.bl4de.EmoticonChat.commands.EC;
 
+import com.servegame.bl4de.EmoticonChat.EmoticonChat;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Text;
 
 /**
  * File: EC.java
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
 public class EC implements CommandExecutor {
+    public EC(EmoticonChat EC){
+
+    }
+
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-        return CommandResult.empty();
+        src.sendMessage(Text.of("Not implemented yet..."));
+        return CommandResult.success();
     }
 }
--- src/main/java/com/servegame/bl4de/EmoticonChat/commands/EC/ECHelp.java
+++ src/main/java/com/servegame/bl4de/EmoticonChat/commands/EC/ECHelp.java
@@ -5,6 +5,7 @@ import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.text.Text;
 
 /**
  * File: ECHelp.java
@@ -13,6 +14,7 @@ import org.spongepowered.api.command.spec.CommandExecutor;
 public class ECHelp implements CommandExecutor {
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-        return CommandResult.empty();
+        src.sendMessage(Text.of("Not implemented yet..."));
+        return CommandResult.success();
     }
 }
